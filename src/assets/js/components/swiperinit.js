44bb8c29e2b643feb773689ccd2733a6f0001cde
Nododigital <nododigital@Matheus-Cruz.local>
2018-04-23 17:35:08 -0300

    final version without adjusts

--- src/assets/js/components/swiperinit.js
+++ src/assets/js/components/swiperinit.js
@@ -3,21 +3,13 @@ $(document).ready(function () {
         slidesPerView: 5,
         spaceBetween: 10,
         centeredSlides: true,
+        initialSlide: 5,
+        slideToClickedSlide: true,
+        loop: true,
         pagination: {
             el: '.swiper-pagination',
             clickable: true
         }
     });
 
-    function cu(className) {
-        var index = 0;
-        $.each($('.swiper-wrapper').children(), function (i, item) {
-            if ($(item).hasClass(className)) {
-                index = i;
-                return false;
-            }
-        });
-        return index;
-    }
-    swiper.slideTo(cu('careca'), 0, !1);
 });
